12e356fbdb140433c6e555a7f37891e56ad5fe00
Riccardo Forina <rforina@redhat.com>
2025-12-27 19:23:30 +0100

    Optimizations

--- src/pages/sitemap.xml.ts
+++ src/pages/sitemap.xml.ts
@@ -1,14 +1,15 @@
 import type { APIRoute } from 'astro';
 
 const pages = [
-  { path: '', changefreq: 'weekly', priority: '1.0' },
-  { path: 'corsi', changefreq: 'monthly', priority: '0.9' },
-  { path: 'regate', changefreq: 'weekly', priority: '0.9' },
-  { path: 'bacheca', changefreq: 'weekly', priority: '0.8' },
-  { path: 'circolo', changefreq: 'monthly', priority: '0.8' },
-  { path: 'meteo', changefreq: 'daily', priority: '0.7' },
-  { path: 'safeguarding', changefreq: 'yearly', priority: '0.5' },
-  { path: 'privacy', changefreq: 'yearly', priority: '0.3' },
+  { path: '', changefreq: 'weekly', priority: '1.0', lastmod: '2025-12-27' },
+  { path: 'corsi', changefreq: 'monthly', priority: '0.9', lastmod: '2025-12-27' },
+  { path: 'regate', changefreq: 'weekly', priority: '0.9', lastmod: '2025-12-27' },
+  { path: 'bacheca', changefreq: 'weekly', priority: '0.8', lastmod: '2025-12-27' },
+  { path: 'galleria', changefreq: 'monthly', priority: '0.8', lastmod: '2025-12-27' },
+  { path: 'circolo', changefreq: 'monthly', priority: '0.8', lastmod: '2025-12-27' },
+  { path: 'meteo', changefreq: 'daily', priority: '0.7', lastmod: '2025-12-27' },
+  { path: 'safeguarding', changefreq: 'yearly', priority: '0.5', lastmod: '2025-12-27' },
+  { path: 'privacy', changefreq: 'yearly', priority: '0.3', lastmod: '2025-12-27' },
 ];
 
 export const GET: APIRoute = ({ site }) => {
@@ -22,6 +23,7 @@ ${pages
   .map(
     (page) => `  <url>
     <loc>${fullUrl}${page.path ? `/${page.path}` : ''}</loc>
+    <lastmod>${page.lastmod}</lastmod>
     <changefreq>${page.changefreq}</changefreq>
     <priority>${page.priority}</priority>
   </url>`
